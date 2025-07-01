029fd0d7243f5c0037155fb79fe6428a149d2a68
Igor Frecautan <frecautan.dev@gmail.com>
2025-07-01 17:11:39 +0300

    add bookingInit mutation

--- src/containers/private/BookingInitContainer/index.tsx
+++ src/containers/private/BookingInitContainer/index.tsx
@@ -1,6 +1,35 @@
 "use client";
+import { useBookingDraft } from "@/hooks/useBookingDraft";
+import { useRouter } from "@/i18n/navigation";
+import { bookingService } from "@/services/booking.service";
+import { useMutation } from "@tanstack/react-query";
 import React from "react";
 
 export const BookingInitContainer = () => {
-  return <div className="loader" />;
+  const { push } = useRouter();
+  const { getBookingDraft, clearBookingDraft } = useBookingDraft();
+
+  const bookingInit = useMutation({
+    mutationFn: bookingService.init,
+    onSuccess: (data) => {
+      clearBookingDraft();
+      push(`/booking/${data?.booking_id}`);
+    },
+    onError: () => {
+      push("/booking");
+    },
+  });
+
+  React.useEffect(() => {
+    const payload = getBookingDraft();
+
+    console.log({ payload });
+    payload && bookingInit.mutate(payload);
+  }, [getBookingDraft]);
+
+  return (
+    <div className="flex justify-center pt-20">
+      <div className="loader" />
+    </div>
+  );
 };
