cc61436b14f695312668650db876fafa3f5bd4f5
bogdanovandreycode <bogdanov.andrey.code@gmail.com>
2025-07-23 14:43:14 +0300

    fix: add missing remote pattern for localhost storage access in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "localhost",
+        port: "8443",
+        pathname: "/storage/**",
+      },
     ],
   },
 };
