dfca1109b66225bbf8e6ce6fa92ca6a132097279
Anton Gadimba <antonhck@gmail.com>
2025-07-24 23:47:22 +0300

    Added CI/CD

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // 👈 ОТКЛЮЧАЕМ sharp
     remotePatterns: [
       {
         protocol: "https",
