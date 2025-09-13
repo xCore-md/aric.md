733db4f3ffc8e83587d63292e8498aea41a49513
Anton Gadimba <antonhck@gmail.com>
2025-09-13 22:24:01 +0300

    Fix build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,6 +13,8 @@ module.exports = {
       out_file: "./logs/out.log",
       error_file: "./logs/error.log",
       log_date_format: "YYYY-MM-DD HH:mm Z",
+      kill_timeout: 5000,
+      listen_timeout: 5000,
     },
   ],
 };
